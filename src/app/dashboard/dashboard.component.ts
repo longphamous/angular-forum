891f82235371de46d8a446662060155241c43cf6
C0d35 <tt195055697699@gmail.com>
2025-08-01 22:27:13 +0200

    AF-0000: improve linting and rules

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-dashboard',
+  selector: "dashboard",
   imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  templateUrl: "./dashboard.component.html",
+  styleUrl: "./dashboard.component.scss",
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
