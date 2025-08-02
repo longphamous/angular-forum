5d11f8b3b624519cd667ae10e5019d20e955e601
C0d35 <tt195055697699@gmail.com>
2025-08-02 02:07:08 +0200

    AF-0000: re-add tailwind css and update components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
+import { NavigationBarComponent } from "./toolbox/navigation-bar/navigation-bar.component";
+
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss"
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
   title = "angular-forum";
---
+++ src/app/toolbox/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { AvatarModule } from "primeng/avatar";
+import { BadgeModule } from "primeng/badge";
+import { MenubarModule } from "primeng/menubar";
+
+@Component({
+  selector: "navigation-bar",
+  imports: [AvatarModule, BadgeModule, MenubarModule],
+  templateUrl: "./navigation-bar.component.html",
+  styleUrl: "./navigation-bar.component.scss",
+})
+export class NavigationBarComponent {
+  items: any = [];
+}
